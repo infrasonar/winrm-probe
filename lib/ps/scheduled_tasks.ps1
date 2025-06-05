f796f49494ca7705f3fc900f5bd97df894ad6936
Jeroen van der Heijden <jeroen@cesbit.com>
2025-06-05 15:07:53 +0200

    scheduled tasks

--- lib/ps/scheduled_tasks.ps1
+++ lib/ps/scheduled_tasks.ps1
@@ -1,11 +1,9 @@
 # Get Scheduled Task Information
 $tasks = Get-ScheduledTask | Get-ScheduledTaskInfo | Select-Object -Property LastRunTime, NextRunTime, LastTaskResult, NumberOfMissedRuns, TaskName, PSComputerName
 
-# Get the current UTC date and time as a DateTimeOffset
-$nowUtc = [DateTimeOffset]::UtcNow
-
-# Calculate ticks from 0001-01-01 to now in seconds
-$secondsSinceYear1 = $nowUtc.Ticks / 10000000
+$unixEpoch = [DateTimeOffset](Get-Date -Year 1970 -Month 1 -Day 1 -Hour 0 -Minute 0 -Second 0 -Millisecond 0 -Kind Utc)
+$unixEpochTicks = $unixEpoch.Ticks
+$unixEpochTicksInSeconds = $unixEpochTicks / 10000000
 
 # Process the tasks and convert dates to Unix timestamps
 # Note we divide by 10.000.000 as there are 10,000,000 nanosecond ticks in one second
@@ -15,7 +13,7 @@ $processedTasks = $tasks | Select-Object -Property @{
         # Check if LastRunTime exists and is a valid DateTime object
         if ($_.LastRunTime -and ($_.LastRunTime -is [DateTime]) -and ($_.LastRunTime -ne [DateTime]::MinValue)) {
             # Convert to DateTimeOffset to easily get Unix milliseconds
-            [int64](([DateTimeOffset]$_.LastRunTime.ToUniversalTime()).Ticks / 10000000 - $secondsSinceYear1)
+            [int64](([DateTimeOffset]$_.LastRunTime.ToUniversalTime()).Ticks / 10000000 - $unixEpochTicksInSeconds)
         } else {
             $null # Or 0, or an empty string, depending on your desired output for null dates
         }
@@ -26,7 +24,7 @@ $processedTasks = $tasks | Select-Object -Property @{
         # Check if NextRunTime exists and is a valid DateTime object
         if ($_.NextRunTime -and ($_.NextRunTime -is [DateTime]) -and ($_.NextRunTime -ne [DateTime]::MinValue)) {
             # Convert to DateTimeOffset, then calculate Unix seconds
-            [int64](([DateTimeOffset]$_.NextRunTime.ToUniversalTime()).Ticks / 10000000 - $secondsSinceYear1)
+            [int64](([DateTimeOffset]$_.NextRunTime.ToUniversalTime()).Ticks / 10000000 - $unixEpochTicksInSeconds)
         } else {
             $null # Or 0, or an empty string
         }
