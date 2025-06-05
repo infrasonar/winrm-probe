6a085e8c50bb7451c0ab0e8ab70dd20b6da08e75
Jeroen van der Heijden <jeroen@cesbit.com>
2025-06-05 14:58:05 +0200

    scheduled tasks

--- lib/ps/scheduled_tasks.ps1
+++ lib/ps/scheduled_tasks.ps1
@@ -4,6 +4,7 @@ $tasks = Get-ScheduledTask | Get-ScheduledTaskInfo | Select-Object -Property Las
 # Define the Unix Epoch start time (January 1, 1970, 00:00:00 UTC)
 # Using [DateTimeOffset] for better handling of UTC and milliseconds
 $unixEpoch = [DateTimeOffset](Get-Date -Year 1970 -Month 1 -Day 1 -Hour 0 -Minute 0 -Second 0 -Millisecond 0 -Kind Utc)
+$unixEpochTicksInSeconds = $unixEpoch.Ticks / 10000000
 
 # Process the tasks and convert dates to Unix timestamps
 # Note we divide by 10.000.000 as there are 10,000,000 nanosecond ticks in one second
@@ -13,7 +14,7 @@ $processedTasks = $tasks | Select-Object -Property @{
         # Check if LastRunTime exists and is a valid DateTime object
         if ($_.LastRunTime -and ($_.LastRunTime -is [DateTime]) -and ($_.LastRunTime -ne [DateTime]::MinValue)) {
             # Convert to DateTimeOffset to easily get Unix milliseconds
-            [DateTimeOffset]$_.LastRunTime.ToUniversalTime().Ticks / 10000000 - ([DateTimeOffset]$unixEpoch.Ticks / 10000000)
+            [int64](([DateTimeOffset]$_.LastRunTime.ToUniversalTime()).Ticks / 10000000 - $unixEpochTicksInSeconds)
         } else {
             $null # Or 0, or an empty string, depending on your desired output for null dates
         }
@@ -23,8 +24,8 @@ $processedTasks = $tasks | Select-Object -Property @{
     Expression = {
         # Check if NextRunTime exists and is a valid DateTime object
         if ($_.NextRunTime -and ($_.NextRunTime -is [DateTime]) -and ($_.NextRunTime -ne [DateTime]::MinValue)) {
-            # Convert to DateTimeOffset to easily get Unix milliseconds
-            [DateTimeOffset]$_.NextRunTime.ToUniversalTime().Ticks / 10000000 - ([DateTimeOffset]$unixEpoch.Ticks / 10000000)
+            # Convert to DateTimeOffset, then calculate Unix seconds
+            [int64](([DateTimeOffset]$_.NextRunTime.ToUniversalTime()).Ticks / 10000000 - $unixEpochTicksInSeconds)
         } else {
             $null # Or 0, or an empty string
         }
