04b146931eff8e7973b4fe0fa000b28d43815fa2
Jeroen van der Heijden <jeroen@cesbit.com>
2025-06-05 15:11:53 +0200

    scheduled tasks

--- lib/ps/scheduled_tasks.ps1
+++ lib/ps/scheduled_tasks.ps1
@@ -1,10 +1,6 @@
 # Get Scheduled Task Information
 $tasks = Get-ScheduledTask | Get-ScheduledTaskInfo | Select-Object -Property LastRunTime, NextRunTime, LastTaskResult, NumberOfMissedRuns, TaskName, PSComputerName
 
-$unixEpoch = [DateTimeOffset](Get-Date -Year 1970 -Month 1 -Day 1 -Hour 0 -Minute 0 -Second 0 -Millisecond 0 -Kind Utc)
-$unixEpochTicks = $unixEpoch.Ticks
-$unixEpochTicksInSeconds = $unixEpochTicks / 10000000
-
 # Process the tasks and convert dates to Unix timestamps
 # Note we divide by 10.000.000 as there are 10,000,000 nanosecond ticks in one second
 $processedTasks = $tasks | Select-Object -Property @{
@@ -13,7 +9,7 @@ $processedTasks = $tasks | Select-Object -Property @{
         # Check if LastRunTime exists and is a valid DateTime object
         if ($_.LastRunTime -and ($_.LastRunTime -is [DateTime]) -and ($_.LastRunTime -ne [DateTime]::MinValue)) {
             # Convert to DateTimeOffset to easily get Unix milliseconds
-            [int64](([DateTimeOffset]$_.LastRunTime.ToUniversalTime()).Ticks / 10000000 - $unixEpochTicksInSeconds)
+            [int64]([DateTimeOffset]$_.LastRunTime.ToUniversalTime()).ToUnixTimeSeconds()
         } else {
             $null # Or 0, or an empty string, depending on your desired output for null dates
         }
@@ -24,7 +20,7 @@ $processedTasks = $tasks | Select-Object -Property @{
         # Check if NextRunTime exists and is a valid DateTime object
         if ($_.NextRunTime -and ($_.NextRunTime -is [DateTime]) -and ($_.NextRunTime -ne [DateTime]::MinValue)) {
             # Convert to DateTimeOffset, then calculate Unix seconds
-            [int64](([DateTimeOffset]$_.NextRunTime.ToUniversalTime()).Ticks / 10000000 - $unixEpochTicksInSeconds)
+            [int64]([DateTimeOffset]$_.LastRunTime.ToUniversalTime()).ToUnixTimeSeconds()
         } else {
             $null # Or 0, or an empty string
         }
